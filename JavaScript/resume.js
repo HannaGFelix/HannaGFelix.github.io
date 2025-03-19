80b1e3c79c1bc49780e1681b8e4015c305b94f49
HannaGFelix <hannag.felix@gmail.com>
2025-03-19 14:44:11 -0400

    Header and Footer Updated

--- JavaScript/resume.js
+++ JavaScript/resume.js
@@ -1,16 +1,14 @@
 
 document.querySelector('.toggle').addEventListener('click', function() {
     const content = document.querySelector('.box');
-    const sign = document.querySelector('.sign'); // To change the + to -
+    const sign = document.querySelector('.sign');
   
-    // Toggle the 'open' class
     content.classList.toggle('open');
-  
-    // Toggle the sign between + and -
+
     if (content.classList.contains('open')) {
-      sign.textContent = '-'; // Change to '-' when the content is shown
+      sign.textContent = '-'; 
     } else {
-      sign.textContent = '+'; // Change to '+' when the content is hidden
+      sign.textContent = '+'; 
     }
   });
   
